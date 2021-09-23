b9ec25706b0ecd9f7cfdf1ab9b0d61b0466db6fd
msillahjawara <msillahjawara@gmail.com>
2021-09-23 17:01:16 +0200

    ejercicio 11

---
+++ app/code/Hiberus/Sillah/Plugin/Mark/MarksPlugin.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Hiberus\Sillah\Plugin\Mark;
+
+use Hiberus\Sillah\Model\Exam;
+
+class MarksPlugin
+{
+
+    public function afterGetMark(Exam $subject, $result)
+    {
+        if ($subject->getData('mark') < 5.0) {
+            $subject->setMark(4.9);
+            $result = $subject->getData('mark');
+            //var_dump($subject->getData());die;
+        }
+        return $result;
+
+    }
+
+
+}
